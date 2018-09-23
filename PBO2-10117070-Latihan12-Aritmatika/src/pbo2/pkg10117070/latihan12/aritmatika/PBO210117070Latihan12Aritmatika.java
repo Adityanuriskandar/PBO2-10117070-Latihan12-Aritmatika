32746b532608085abf96fc9ebb01594d96a22566
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-09-23 12:49:41 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan12-Aritmatika/src/pbo2/pkg10117070/latihan12/aritmatika/PBO210117070Latihan12Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan12.aritmatika;
+
+/**
+ *
+ * @author Aditya
+ * NAMA     : Aditya Nur Iskandar
+ * KELAS    : PBO2/IF2
+ * NIM      : 10117070
+ * Deskripsi Program : Program ini berisi program untuk mengetahui penggunaan
+ * Aritmatika dalam bahasa Java.
+ */
+public class PBO210117070Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = "+(a+b));
+        System.out.println("a - b = "+(a-b));
+        System.out.println("a * b = "+(a*b));
+        System.out.println("b / a = "+(b/a));
+        System.out.println("b % a = "+(b%a));
+        System.out.println("c % a = "+(c%a));
+        System.out.println("a++ ="+(a++));
+        System.out.println("b-- ="+(a--));
+        System.out.println("d++ ="+(d++));
+        System.out.println("++d ="+(++d));
+    }
+    
+}
